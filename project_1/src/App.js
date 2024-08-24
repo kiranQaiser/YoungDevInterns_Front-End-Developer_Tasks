174dc7df054d9e8882cf5be39bcc729644987aa9
Kiran <128803881+kiranQaiser@users.noreply.github.com>
2024-08-24 21:24:40 +0000

    final project

---
+++ project_1/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import RecipeSearch from "./RecipeSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe Finder</h1>
+      <RecipeSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ project_1/src/api.js
@@ -0,0 +1,3 @@
+// src/api.js
+export const API_KEY = "f0553273b9854f846285cbfa14140567"; // Your API key
+export const BASE_URL = "https://api.spoonacular.com/recipes/complexSearch";
